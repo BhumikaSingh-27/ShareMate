71f305d9973b9459234b9cc3ff75ee18eac4e408
BhumikaSingh-27 <100286699+BhumikaSingh-27@users.noreply.github.com>
2023-06-12 11:41:37 +0530

    style: component created

--- src/App.js
+++ src/App.js
@@ -1,23 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+import { Route, Routes } from "react-router-dom";
+import "./App.css";
+import Mockman from "mockman-js";
+import Navbar from "./components/Navbar/Navbar";
+import Home  from "./pages/Home/Home";
+import UserList from "./components/UserList/UserList";
+import PostCard from "./components/PostCard/PostCard";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {/* <Home /> */}
+      {/* <Navbar /> */}
+      {/* <PostCard /> */}
+<UserList />
+
+      <Routes>
+        <Route path="/mockman" element={<Mockman />}></Route>
+      </Routes>
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
+import { BrowserRouter } from "react-router-dom";
 import reportWebVitals from "./reportWebVitals";
 import { makeServer } from "./server";
 
@@ -11,7 +12,9 @@ makeServer();
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
+    <BrowserRouter>
     <App />
+    </BrowserRouter>
   </React.StrictMode>
 );
 
